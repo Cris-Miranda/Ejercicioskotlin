187113a8a8e0772b68c9bb98a730e9a001b5135d
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-11-02 12:01:20 -0600

    commit quince

--- Ejercicio1.kt
+++ Ejercicio1.kt
@@ -13,4 +13,5 @@ fun main(parametro: Array<String>) {
         println("Dale otro commit")
         println("Dale otro commit")
         println("Dale otro commit")
+        println("Dale otro commit")
 }
